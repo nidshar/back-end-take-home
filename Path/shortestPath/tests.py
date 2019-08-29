38698a9505fa436f69479f1eac2e4156c1e4994a
Nidhi <nidhi.catch@gmail.com>
2019-08-29 00:13:35 -0400

    addded test cases

--- Path/shortestPath/tests.py
+++ Path/shortestPath/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+        
+@pytest.mark.parametrize(
+    "source,destination,expected_result",
+    [
+        ("YYZ","JFK","YYZ -> JFK"),
+        ("YYZ","YVR","YYZ -> JFK -> LAX -> YVR"),
+        ("YYZ","ORD","No Route"),
+        ("XXX","YYZ","Invalid Origin"),
+        ("ORD","XXX","Invalid Destination")
+    ]
+)
+def test_path(db,client,source,destination,expected_result):
+    response = client.get(f"/path/?source={source}&destination={destination}")
+    assert response.status_code == 200
+    assert response.content.decode("utf-8") == expected_result
+    
+    
